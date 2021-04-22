6a85a045bd77d0859e37ee045f7d5ba127f41244
abdy-sanchez <abdy.sanchez@udea.edu.co>
2021-04-21 20:31:27 -0500

    ---[AVANCE]--- más codigo, inicializacion del serial y comienzo para la comunicación

--- Parcial1_Informa2.ino
+++ Parcial1_Informa2.ino
@@ -1,5 +1,5 @@
 //ARREGLO LETRA A
-byte A[] = {B00011000,B00111100,B01100110,B01100110,B01111110,B01111110,B01100110,B01100110};
+byte A[] = {B01100110,B01100110,B01100110,B00111100,B00011000,B00111100,B01100110,B01100110};
 
 
 
@@ -18,7 +18,7 @@ byte A[] = {B00011000,B00111100,B01100110,B01100110,B01111110,B01111110,B0110011
 
 //SER
 int pinData = 5 ; 
-//RCLK
+//RCLK [Buffer de salida]
 int pinLatch = 6 ;
 //SRCLK
 int pinClock = 7 ;
@@ -30,33 +30,61 @@ int pinClock = 7 ;
 //Funcion para administar los datos y encender o apagar los leds
 void leds( int f1, int f2, int f3, int f4, int f5, int f6, int f7, int f8 ){
 
+  //Las funciones shiftOut nos sirven para despazar la informacion (1,0) por los diferentes Microcontroladores
+  
   shiftOut(pinData, pinClock, LSBFIRST, f8);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f7);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f6);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f5);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f4);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f3);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f2);
+  
   shiftOut(pinData, pinClock, LSBFIRST, f1);
-  digitalWrite(pinLatch, HIGH);
-  digitalWrite(pinLatch, LOW);
+  
+  digitalWrite(pinLatch, HIGH);		//Con las siguientes dos lineas mostramos lo que hay almacenado en el registro
+  digitalWrite(pinLatch, LOW);		//Se genera un pulso, basicamente
 
+  //las variable f1 a f8 representan las filas de la matriz
+  
 }
 
 
 
 void setup(){
   
+  Serial.begin(9600);		//Iniciamos el Serial
+  
   //Inicializacion de pines en modo OUTPUT
    pinMode(pinData, OUTPUT);
    pinMode(pinLatch, OUTPUT);
-   pinMode(pinClock, OUTPUT); 
+   pinMode(pinClock, OUTPUT);
+  
+  //IMPRIMIMOS EN ESERIAL LAS OPCIONES PARA EL USUARIO
+  
+  Serial.println(" ---BIENVENIDO---");
+  Serial.println(" > Que desea hacer? ");
+  Serial.println(" Opciones: [0] Verificar el Sistema de LEDS ; [1] Definir un patron  ; [2] Mostrar un patron completamente PERSONALIZADO ");
+    
 
 }
 
 void loop(){
   
-
-  leds(A[0] ,A[1] ,A[2] ,A[3] ,A[4] ,A[5] ,A[6] , A[7]);
+  //Ciclo que se repite indefinidamente hasta que se ingrese algo en el Monitor Serial
+  while( Serial.available()==0 ){
+  Serial.println(" Su Respuesta: ");
+  delay(10000);
+  
+  } //
+  
+	//IMPRESION DE LA LETRA
+  //leds(A[0] ,A[1] ,A[2] ,A[3] ,A[4] ,A[5] ,A[6] , A[7]);
 
 }
